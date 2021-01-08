3bc5f5545432b18cae8d0fe4f586ea835b9ef1a5
William Chin <gotem4ever1@gmail.com>
2021-01-08 11:04:31 +1100

    third

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 //Set up a variable for gameover
-//players (X or O, who going first)
+//Set up players (X or O, who going first)
 var gameOver = false;
-<<<<<<< HEAD
-var player = '0'
-if (confirm('Is 0 goes first?') === true) {
-  player = '0';
-=======
+
+
 var player = 'O'
 if (confirm('Is O goes first?') === true) {
   player = 'O';
->>>>>>> dcf4a338351e19ed141e87454129225ab17211d4
+
 } else {
   player = 'X';
 }
@@ -38,56 +35,34 @@ var winningCombinations = [
   [2, 4, 6]
 ];
 
-squares = []; //reset variable
+//reset variable to empty array
+//reset variable for the game being over, make it equals to false
 
-gameOver = false; //reset variable
+squares = [];
+gameOver = false;
 
-function squareClick (square) { //set up function squareClick with square
-  var idElement = document.getElementById(square).value;//variable equals to html id parameter with value
-  var parseSquare = ((parseInt(square.substring(3, 4))) - 1); //variable equals to js parseInt which shows the index of the array of squares when user clicks
-  if (idElement === '') {  //if variable is equal to explicit string
-    document.getElementById(square).value = player;//html element shows when parameter square id's value equals to player
-    squares[parseSquare] = player; //update the element to variable player
+//set up function squareClick with square.
+//set up variable equals to html id parameter with value.
+//set up a variable that is set equal to the JavaScript method call
+//parseInt() passing as an argument square.substring(3, 4),
+// subtract one from the result of the parseInt method call (represent the index of the array of squares where the user clicked)
+
+function squareClick (square) {
+  var idElement = document.getElementById(square).value;
+  var parseSquare = ((parseInt(square.substring(3, 4))) - 1);
+  if (idElement === '') { //if variable is equal to explicit string
+    document.getElementById(square).value = player; //html element shows when parameter square id's value equals to player
+    squares[parseSquare] = player; //update the element at the index to variable player
     checkAndSwap();
   }
 }
 
 function checkAndSwap () {
   checkForWinner('X'); //setup function seperate between players
-<<<<<<< HEAD
-  !gameOver && checkForWinner('0');
 
-  if (!gameOver) {
-    if (player === '0')
-      player = 'X';
-    else {
-      player = '0';
-    }
-  }
-}
-
-function playAgain () {  //set up function for playagain
-  var response = confirm('Play Again?');
-  if (response === true) {
-    alert('let\'s Play!');
-    location.reload();
-  } else {
-    alert('Thanks for playing!');
-  }
-}
-
-function checkForWinner (value) { //set up function for checkforwinner with value
-  for (var i = 0; i < winningCombinations.length; i++) { //statement for determine the winner
-    if (squares[winningCombinations[i][0]] === value && squares[winningCombinations[i][1]] === value && squares[winningCombinations[i][2]] === value) {
-      alert(value + ' Won the Game!');
-      gameOver = true;
-    }
-  }
-
-=======
   !gameOver && checkForWinner('O');
-  
-  if (!gameOver) {
+
+  if (!gameOver) {   //checks if the player variable is equal to O, if true, set player to X
     if (player === 'O')
       player = 'X';
     else {
@@ -112,10 +87,10 @@ function checkForWinner (value) { //set up function for checkforwinner with valu
       alert(value + ' Won the Game!');
       gameOver = true;
     }
+
   }
 
->>>>>>> dcf4a338351e19ed141e87454129225ab17211d4
-  if (gameOver === true) { //check gameover and call function playagain
+if (gameOver === true) { //check gameover and call function playagain
     playAgain();
   }
 }
